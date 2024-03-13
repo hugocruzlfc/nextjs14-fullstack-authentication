3f038bd2b3c1118e9280ae9505eb339abf950796
hugocruzlfc <hugocruzlfc@gmail.com>
2024-03-13 18:29:26 +0100

    fix: update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,34 +3,22 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const pathVariable = request.nextUrl.pathname;
-  // const publicPaths = [
-  //   "/update-password",
-  //   "/login",
-  //   "/register",
-  //   "/forgot-password",
-  // ];
+  const publicPaths = [
+    "/update-password",
+    "/login",
+    "/register",
+    "/forgot-password",
+  ];
 
   const auth = request.cookies.get("token")?.value;
 
-  if (auth && !request.nextUrl.pathname.startsWith("/dashboard")) {
-    return Response.redirect(new URL("/dashboard", request.url));
+  if (auth && publicPaths.includes(pathVariable)) {
+    return Response.redirect(new URL("/", request.url));
   }
 
-  if (!auth && !request.nextUrl.pathname.startsWith("/login")) {
+  if (!auth && !publicPaths.includes(pathVariable)) {
     return Response.redirect(new URL("/login", request.url));
   }
-
-  // console.log(auth);
-
-  // const isLogged = publicPaths.includes(pathVariable) && auth;
-
-  // if (isLogged) {
-  //   return NextResponse.redirect(new URL("/", request.url));
-  // }
-
-  // if (!isLogged) {
-  //   return NextResponse.redirect(new URL("/login", request.url));
-  // }
 }
 
 export const config = {
